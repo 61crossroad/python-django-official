9c604730d69f9eaf9aa2a7ba4479d7f861970dcf
Kim Seunghwan <61crossroad@gmail.com>
2026-08-14 23:44:46 +0900

    using generic view

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,16 +1,17 @@
 import logging
 
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 # from django.http import Http404
 # from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
-# Like a Service Class in Spring
+# Like a Controller + Service Class in Spring
 logger = logging.getLogger('django')
 
 def index(request):
@@ -20,6 +21,13 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     # return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 def detail(request, question_id):
     '''
@@ -33,9 +41,17 @@ def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
     return render(request, "polls/detail.html", {"question": question})
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 '''
 def vote(request, question_id):
