c9e3d56e4f659542686e58ca4688236154f25640
Kim Seunghwan <61crossroad@gmail.com>
2026-08-22 02:19:11 +0900

    admin foreign key customize

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
+
+# class ChoiceInLine(admin.StackedInline):
+# abbreviated version
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     # have to chage the name fields -> fieldsets
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
 
 # Register your models here.
